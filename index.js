73116337a707c0c343cac3723a607d41a1f1efd3
Jeremy Fleming <thevisionmachine@gmail.com>
2023-11-10 17:49:47 -0600

    fixed tabBtn bug

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ deleteBtn.addEventListener("dblclick", function() {
 
 tabBtn.addEventListener("click", function() {
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
