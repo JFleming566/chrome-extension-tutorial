10d6d23ef500e811cec511152a38f7fa2a785fc3
Jeremy Fleming <thevisionmachine@gmail.com>
2023-11-10 08:58:58 -0600

    tabs button function bug

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ deleteBtn.addEventListener("dblclick", function() {
 
 tabBtn.addEventListener("click", function() {
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function getTabs(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function () {
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
